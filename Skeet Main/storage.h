1a1f2945e706e6b98f8d5787e2d5a1550428c18c
Dallin Olson <86103274+DallinO@users.noreply.github.com>
2023-06-08 12:28:11 -0600

    StorageTime & StorageGun Updated

--- Skeet Main/storage.h
+++ Skeet Main/storage.h
@@ -2,6 +2,13 @@
 #include "storageElement.h"
 #include "iterator.h"
 #include <vector>
+#include <array>
+#include <assert.h>
+
+#define FRAMES_PER_SECOND 30
+
+using namespace std;
+
 class Storage
 {
 private:
@@ -30,10 +37,27 @@ public:
 
 class StorageTime
 {
-
+private:
+	int framesLeft;
+	int levelNumber;
+	// length in seconds of each level
+	array<int, 5> levelLength;
+	// seconds from frames
+	int secondsFromFrames(int frame) const
+	{
+		return frame / FRAMES_PER_SECOND;
+	}
+	// how long have we been in the level in seconds?
+	int secondsInLevel() const
+	{
+		assert(levelNumber < (int)levelLength.size());
+		return levelLength[levelNumber] - secondsFromFrames(framesLeft);
+	}
 };
 
 class StorageGun
 {
-
+private:
+	double angle;
+	Point pt;
 };
