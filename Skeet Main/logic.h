3a5a233d8cb1c99b8afe54615c6834bd313bb764
Dallin Olson <86103274+DallinO@users.noreply.github.com>
2023-06-08 11:44:05 -0600

    Stub Logic Update

--- Skeet Main/logic.h
+++ Skeet Main/logic.h
@@ -1,5 +1,21 @@
 #pragma once
+#include "storage.h"
 class Logic
 {
+private:
+	Storage storage;
+public:
+	Logic();
+	void advance();
+	void spawn();
+	void input();
+	iterator begin();
+	iterator end();
+	int getFramesLeft();
+	int getLevelNumber();
+	double getGunAngle();
+	bool isPlaying();
+	bool isGameOver();
+	double getPercentLeft();
 };
 
