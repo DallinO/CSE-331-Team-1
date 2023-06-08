bfc829c095a1f8c98160d74337c373229444906f
Dallin Olson <86103274+DallinO@users.noreply.github.com>
2023-06-08 13:34:13 -0600

    InterfaceElement Updated

--- Skeet Main/interfaceElement.cpp
+++ Skeet Main/interfaceElement.cpp
@@ -28,7 +28,7 @@ inline void glVertexPoint(const Point& point)
 	glVertex2f((GLfloat)point.getX(), (GLfloat)point.getY());
 }
 
-void InterfaceBullet::drawDot(const Point& point, double radius = 2.0, double red = 1.0, double green = 1.0, double blue = 1.0) const
+void drawDot(const Point& point, double radius = 2.0, double red = 1.0, double green = 1.0, double blue = 1.0)
 {
 	// Get ready, get set...
 	glBegin(GL_TRIANGLE_FAN);
@@ -46,8 +46,8 @@ void InterfaceBullet::drawDot(const Point& point, double radius = 2.0, double re
 	glEnd();
 }
 
-void InterfaceBullet::drawLine(const Point& begin, const Point& end,
-	double red, double green, double blue) const
+void drawLine(const Point& begin, const Point& end,
+	double red, double green, double blue)
 {
 	// Get ready...
 	glBegin(GL_LINES);
@@ -62,8 +62,73 @@ void InterfaceBullet::drawLine(const Point& begin, const Point& end,
 	glEnd();
 }
 
+void drawDisk(const Point& center, double radius,
+	double red, double green, double blue)
+{
+	assert(radius > 1.0);
+	const double increment = M_PI / radius;  // bigger the circle, the more increments
+
+	// begin drawing
+	glBegin(GL_TRIANGLES);
+	glColor3f((GLfloat)red /* red % */, (GLfloat)green /* green % */, (GLfloat)blue /* blue % */);
+
+	// three points: center, pt1, pt2
+	Point pt1;
+	pt1.setX(center.getX() + (radius * cos(0.0)));
+	pt1.setY(center.getY() + (radius * sin(0.0)));
+	Point pt2(pt1);
+
+	// go around the circle
+	for (double radians = increment;
+		radians <= M_PI * 2.0 + .5;
+		radians += increment)
+	{
+		pt2.setX(center.getX() + (radius * cos(radians)));
+		pt2.setY(center.getY() + (radius * sin(radians)));
+
+		glVertex2f((GLfloat)center.getX(), (GLfloat)center.getY());
+		glVertex2f((GLfloat)pt1.getX(), (GLfloat)pt1.getY());
+		glVertex2f((GLfloat)pt2.getX(), (GLfloat)pt2.getY());
+
+		pt1 = pt2;
+	}
+
+	// complete drawing
+	glEnd();
+}
+
 void InterfacePellet::draw(StorageElement* element)
 {
 	if (!element->isDead())
 		drawDot(element->getPoint(), 3.0, 1.0, 1.0, 0.0);
+}
+
+void InterfaceShrapnel::draw(StorageElement* element)
+{
+	if (!element->isDead())
+		drawDot(element->getPoint(), element->getRadius(), 1.0, 1.0, 0.0);
+}
+
+void InterfaceBomb::draw(StorageElement* element)
+{
+	if (!element->isDead())
+	{
+		// Bomb actually has a gradient to cut out the harsh edges
+		drawDot(element->getPoint(), element->getRadius() + 2.0, 0.50, 0.50, 0.00);
+		drawDot(element->getPoint(), element->getRadius() + 1.0, 0.75, 0.75, 0.00);
+		drawDot(element->getPoint(), element->getRadius() + 0.0, 0.87, 0.87, 0.00);
+		drawDot(element->getPoint(), element->getRadius() - 1.0, 1.00, 1.00, 0.00);
+	}
+}
+
+void InterfaceMissle::draw(StorageElement* element)
+{
+	if (!element->isDead())
+	{
+		// missile is a line with a dot at the end so it looks like fins.
+		Point ptNext(element->getPoint());
+		ptNext.add(element->getVelocity());
+		drawLine(element->getPoint(), ptNext, 1.0, 1.0, 0.0);
+		drawDot(element->getPoint(), 3.0, 1.0, 1.0, 1.0);
+	}
 }
--- Skeet Main/interfaceElement.h
+++ Skeet Main/interfaceElement.h
@@ -1,24 +1,12 @@
 #pragma once
-#include "storageElement.h"
 
 class InterfaceElement
 {
 public:
-	virtual void draw(StorageElement* element) = 0;
+	void virtual draw(StorageElement* element) = 0;
 };
 
-class InterfaceBullet : public InterfaceElement
-{
-protected:
-	void drawLine(const Point& begin, const Point& end,
-		double red = 1.0, double green = 1.0, double blue = 1.0) const;
-	void drawDot(const Point& point, double radius = 2.0,
-		double red = 1.0, double green = 1.0, double blue = 1.0) const;
-	int random(int min, int max);
-	double random(double min, double max);
-};
-
-class InterfacePellet : public InterfaceBird
+class InterfacePellet : public InterfaceElement
 {
 public:
 	void draw(StorageElement* element) override;
